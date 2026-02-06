f25f53949218873927b51c1f60d64790c4e4bdd6
haroon64 <haroonsiddique97@gmail.com>
2026-02-07 00:49:11 +0500

    navbar with nav elements created also configured some urls ; ;

--- store_shop/store_shop/urls.py
+++ store_shop/store_shop/urls.py
@@ -2,8 +2,17 @@
 from django.contrib import admin
 from django.urls import path
 from core import views
-
+from django.conf import settings
+from django.urls import include
 urlpatterns = [
     path('', views.index, name='index'),
+    path('contact/', views.contact, name='contact'),
     path('admin/', admin.site.urls),
 ]
+
+
+if settings.DEBUG:
+    # Include django_browser_reload URLs only in DEBUG mode
+    urlpatterns += [
+        path("__reload__/", include("django_browser_reload.urls")),
+    ]
