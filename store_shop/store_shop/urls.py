2db2ec65c4be1d9d486ac0bb2e9bacbe612db009
haroon64 <haroonsiddique97@gmail.com>
2026-02-10 16:13:50 +0500

    configured core urls

--- store_shop/store_shop/urls.py
+++ store_shop/store_shop/urls.py
@@ -7,8 +7,7 @@ from django.conf import settings
 from django.urls import include
 from django.conf.urls.static import static  
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('contact/', views.contact, name='contact'),
+    path('core/', include('core.urls')),
     path('items/', include('item.urls')),
     path('admin/', admin.site.urls),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
